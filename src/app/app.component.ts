7a9e33821a5cf291a8ef2c2b124d138a640332fe
Sh1ro0o <david.pintar00@gmail.com>
2021-01-18 13:43:21 +0100

    actual fix (fixed methods and changed onclick event)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,19 @@ export class AppComponent implements OnInit {
 
   title = "Reddit Titles";
   loading = false;
-  readonly ROOT_URL_HOT = 'https://www.reddit.com/hot/.json';
-  readonly ROOT_URL_NEW = 'https://www.reddit.com/new/.json';
   posts: any;
   decidedSearch = "hot"
 
-  async getPosts(search: ("hot" | "new" | "top") = "hot") {
+  setDecidedSearchAndGetPosts (decidedSearch: 'hot' | 'new' | 'top')
+  {
+    this.decidedSearch = decidedSearch;
+    this.getPosts();
+  }
+
+  async getPosts() {
     this.loading = true;
     try {
-      const response = await fetch(`https://www.reddit.com/${search}/.json`);
+      const response = await fetch(`https://www.reddit.com/${this.decidedSearch}/.json`);
       console.log(response);
       this.posts = (await response.json()).data.children;
       console.log(this.posts);
