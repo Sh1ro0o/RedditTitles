b6a37dc4f3273dd8a9afa53ef22c6d1294728c08
Sh1ro0o <david.pintar00@gmail.com>
2021-01-18 13:19:00 +0100

    implemented api call to reddit front page, created filter (new, hot, top), improved visual display.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
   title = "Reddit Titles";
-  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com/posts';
+  loading = false;
+  readonly ROOT_URL_HOT = 'https://www.reddit.com/hot/.json';
+  readonly ROOT_URL_NEW = 'https://www.reddit.com/new/.json';
   posts: any;
+  decidedSearch = "hot"
 
-  constructor(private http: HttpClient) { }
-
-  getPosts() {
-    this.posts = this.http.get(this.ROOT_URL)
+  async getPosts(search: "hot" | "new" = "hot") {
+    this.loading = true;
+    try {
+      const response = await fetch(`https://www.reddit.com/${search}/.json`);
+      console.log(response);
+      this.posts = (await response.json()).data.children;
+      console.log(this.posts);
+    }
+    catch(err) {
+      throw err;
+    }
+    finally {
+      this.loading = false;
+    }
   }
 }
